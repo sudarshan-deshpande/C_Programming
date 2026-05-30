d8fb5daf22bceb37e3a04e201e5c34b1833ae84f
Sudarshan Deshpande <sudarshandeshpande2@gmail.com>
2026-05-30 22:51:49 +0530

    30-05-2026

---
+++ Arrays_detailed/Problem_5/problem5.c
@@ -0,0 +1,87 @@
+/*
+Problem 5 — Find Smallest Element in Array
+
+Objective:
+Write a C program to:
+- accept n elements into an integer array
+- validate array size (1 to 100)
+- find the smallest element using traversal
+- display the minimum value
+
+Concepts Practiced:
+- array traversal
+- comparison logic
+- minimum tracking
+- loop mechanics
+- boundary validation
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int input(int *arr, int n);
+int smallest(int *arr, int n);
+
+int input(int *arr, int n)
+{
+    for (int *ptr = arr; ptr < arr + n; ptr++)
+    {
+        if (scanf("%d", ptr) != 1)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int smallest(int *arr, int n)
+{
+    int smallest = arr[0];
+    for (int *ptr = arr + 1; ptr < arr + n; ptr++)
+    {
+        if (*ptr < smallest)
+        {
+            smallest = *ptr;
+        }
+    }
+    return smallest;
+}
+
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    if (scanf("%d", &n) != 1)
+    {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    if (n < 1 || n > 100)
+    {
+        printf("Invalid input\n");
+        return 0;
+    }
+    int *arr = malloc(n * sizeof(int));
+    if (arr == NULL)
+    {
+        printf("No memory allocated\n");
+        return 1;
+    }
+
+    printf("Enter elements: ");
+    if (input(arr, n) != 0)
+    {
+        printf("Invalid input\n");
+        free(arr);
+        arr = NULL;
+        return 1;
+    }
+
+    printf("Smallest = %d\n", smallest(arr, n));
+
+    free(arr);
+    arr = NULL;
+
+    return 0;
+}
